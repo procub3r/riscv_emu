15d3a4f0d80ce1e9786fb83d4dfc3a1a9e909e57
procub3r <procub3r@protonmail.com>
2023-07-01 10:12:42 +0530

    Start implementing instructions + improve logging

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,14 @@
 const std = @import("std");
 const rv = @import("riscv_core.zig");
 
+pub const std_options = struct {
+    pub const log_level = .debug;
+};
+
 pub fn main() !void {
     var core = rv.Core.init();
-    core.x[24] = 0x7331;
+    core.execute(0b00110010101000101000001010010011);
+    core.dump();
+    core.execute(0b00110010101100101010001110010011);
     core.dump();
 }
--- src/riscv_core.zig
+++ src/riscv_core.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+const dump_log = std.log.scoped(.dump);
+const decode_log = std.log.scoped(.decode);
+
 pub const xlen = 32;
 pub const Register = std.meta.Int(.unsigned, xlen);
 
@@ -12,23 +15,42 @@ pub const Core = struct {
     const Self = @This();
 
     pub fn init() Self {
-        var core = Self{
-            .pc = 0,
-            .x = .{0} ** reg_count,
-        };
-        return core;
+        return Self{ .pc = 0, .x = .{0} ** reg_count };
+    }
+
+    pub fn execute(self: *Self, instr: u32) void {
+        const opcode = instr & 0x7f;
+        switch (opcode) {
+            // register immediate instructions
+            0b0010011 => {
+                const rd = (instr & 0xf80) >> 7;
+                const funct3 = (instr & 0x7000) >> 12;
+                const rs1 = (instr & 0xf8000) >> 15;
+                const imm: u12 = @truncate((instr & 0xfff00000) >> 20);
+                switch (funct3) {
+                    0b000 => {
+                        decode_log.info("addi x{d}, x{d}, 0x{x:0>4}", .{ rd, rs1, imm });
+                        self.x[rd] = self.x[rs1] +% imm;
+                    },
+                    0b010 => {
+                        decode_log.info("slti x{d}, x{d}, 0x{x:0>4}", .{ rd, rs1, imm });
+                        const rs1_signed: i32 = @bitCast(self.x[rs1]);
+                        const imm_signed: i12 = @bitCast(imm);
+                        self.x[rd] = @intFromBool(rs1_signed < imm_signed);
+                    },
+                    else => {},
+                }
+            },
+            else => {},
+        }
     }
 
     // dump the state of the core to the console
     pub fn dump(self: *Self) void {
-        std.debug.print("register dump:\n", .{});
+        dump_log.debug("register dump", .{});
         var i: usize = 0;
-        while (i < 8) : (i += 1) {
-            var j: usize = 0;
-            while (j < 4) : (j += 1) {
-                std.debug.print("x{d:<2}: 0x{x:0>8}\t", .{ i * 4 + j, self.x[i * 4 + j] });
-            }
-            std.debug.print("\n", .{});
+        while (i < 16) : (i += 1) {
+            dump_log.debug("x{d:<2}= 0x{x:0>4}  " ** 2, .{ i, self.x[i], 16 + i, self.x[16 + i] });
         }
     }
 };
